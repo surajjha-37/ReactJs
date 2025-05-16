5670efe3b03f93b58f683ab16bb494eb5bb85b44
surajjha-37 <diveshchoudhary497@gmail.com>
2025-05-16 18:38:38 +0530

    Add files via upload

---
+++ greeting.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Greeting() {
+  return (
+    <div>
+      <h1>Hi There, I'm SURAJ.</h1>
+      <p>This is my First Vite Project.</p>
+      <ul>
+        <li>Coding</li>
+        <li>Good Learner</li>
+        <li>Artist</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Greeting;
